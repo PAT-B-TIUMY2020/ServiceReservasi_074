6cfb063e4bd5770b170d5c5d6ccb8f1c8273818f
eeRha <arrasyidtz3@gmail.com>
2020-12-13 21:26:57 +0700

    Add service register dan login

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -29,6 +29,21 @@ namespace ServiceReservasi
         [OperationContract]
         List<Pemesanan> Pemesanan();
 
+        [OperationContract]
+        string Login(string username, string password);
+
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+
+        [OperationContract]
+        string DeleteRegister(string username);
+
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
         // TODO: Add your service operations here
     }
 
@@ -73,5 +88,21 @@ namespace ServiceReservasi
         public int JumlahPemesanan { get; set; }
         [DataMember]
         public string Lokasi { get; set; }
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 }
